af5e687bae16401fce1b1bfce4686280b5148d84
李光辉 <>
2019-05-24 23:58:24 +0800

    添加连接redis连接工具

---
+++ src/db/redis.js
@@ -0,0 +1,45 @@
+const redis = require('redis');
+const { REDIS_CONF } = require('../conf/db');
+
+// 创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+redisClient.on('error', err => {
+  console.error(err);
+});
+
+// redis 读取值（异步过程）
+function get(key) {
+  const promise = new Promise((resolve, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        return reject(err);
+      }
+
+      // 处理key值在redis不存在时,取出的val也是null
+      if(val === null){
+        return resolve(null);
+      }
+
+      //尝试将取出的值解析为对象，若不是对象，则为字符串
+      try{
+        resolve(JSON.parse(val));
+      }catch(err1){
+        resolve(val);
+      }
+    });
+  });
+  return promise;
+}
+
+// redis 设置值（同步过程）???
+function set(key, val) {
+  if (typeof val === 'object') {
+    val = JSON.stringify(val);
+  }
+  redisClient.set(key, val, redis.print);
+}
+
+module.exports = {
+  get,
+  set
+};
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+// 获取 cookie 的过期时间(设置cookie的过期时间为一天之后)
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  console.log('d.toGMTString===', d.toGMTString());
+  return d.toGMTString();
+};
+
+module.exports = {
+  getCookieExpires
+}
